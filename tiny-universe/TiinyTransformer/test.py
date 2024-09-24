d99ef0b79dd20a16a64fef368e96b3692e68a30f
wangzhefeng <wangzhefengr@163.com>
2024-09-25 00:17:28 +0800

    u pdate tiny-universe transformers

--- tiny-universe/TiinyTransformer/test.py
+++ tiny-universe/TiinyTransformer/test.py
@@ -20,14 +20,53 @@ import sys
 ROOT = os.getcwd()
 if str(ROOT) not in sys.path:
     sys.path.append(str(ROOT))
+from dataclasses import dataclass
+
+import torch
+from tiny_transformer import Transformer
 
 # global variable
 LOGGING_LABEL = __file__.split('/')[-1][:-3]
 
+# 创建模型配置文件
+@dataclass
+class TransformerConfig:
+    block_size: int = 1024
+    vocab_size: int = 50304
+    n_layer: int = 4
+    n_head: int = 4
+    n_embd: int = 768
+    dropout: float = 0.0
+    bias: bool = True
+
+model_config = TransformerConfig(
+    block_size = 12, 
+    vocab_size = 10, 
+    n_layer = 2, 
+    n_head = 4, 
+    n_embd = 16, 
+    dropout = 0.0, 
+    bias = True,
+)
 
 
+# 创建模型
+model = Transformer(model_config)
 
 
+# 向前传递
+idx = torch.randint(1, 10, (4, 8))
+logits, _ = model(idx)
+print(f"logits {logits.size()}")
+
+
+# 模型推理
+result = model.generate(idx, 3)
+print(f"generate result {result.size()}")
+
+
+# 生成结果
+print(result)
 
 
 # 测试代码 main 函数
